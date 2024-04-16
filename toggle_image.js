c6d41f98d19cf846e0ef74cab5d8a8afcf33d50e
fenec_fox <atimevil@gmail.com>
2024-04-16 15:50:56 +0900

    다른 방식으로 사진 클릭시 내용 불러오기

--- toggle_image.js
+++ toggle_image.js
@@ -1,8 +1,11 @@
-function getText(event)  {
-  const imageId = event.target.id;
-  const query = 'label[for="'+ imageId + '"]'
-  const text = 
-        document.querySelector(query).innerText;
-  
-  document.getElementById('result').innerText = text;
+function showImageContent(imageId) {
+  // 클릭된 이미지의 id를 이용하여 해당 내용을 보여줌
+  var contentId = imageId + 'content';
+  // 모든 이미지 내용을 숨김
+  $('.memberimage').each(function() {
+      var contentId = $(this).attr('id') + 'content';
+      $('#' + contentId).addClass('hidden');
+  });
+  // 클릭된 이미지에 해당하는 내용을 보여줌
+  $('#' + contentId).removeClass('hidden');
 }
