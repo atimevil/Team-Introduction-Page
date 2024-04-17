22658a3adb878648a5def414ba6437af04211d2b
hangeunhyeong <hgh7546@naver.com>
2024-04-17 17:11:37 +0900

    주석 수정

--- main.js
+++ main.js
@@ -39,16 +39,16 @@ $('#kevinProfileImg').click(() => {
 });
 
 
-/*화면상단이동버튼 시작*/
+/*화면상단이동버튼*/
 
 
 const $Top_btn = document.querySelector(".MoveTop_btn");
 
 //버튼 클릭시 페이지 상단으로 이동
-$Top_btn.onclick=()=>{
+$Top_btn.onclick = () => {
     window.scrollTo({
         top: 0,    //페이지 총 Height
         behavior: "smooth"
     });
 };
-/*화면상단이동버튼 끝*/
+
