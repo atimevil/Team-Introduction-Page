15411344a2fd44ab3d88f062409c362a2da9db5e
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-04-18 01:34:24 +0900

    정근 팀장(feat.원하  &  규리)  완성

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+
+
 $(document).ready(function () {
-    
     $('#carouselExampleIndicators').carousel({
         interval: 5000,
         ride: "carousel"
@@ -10,18 +11,28 @@ $('.profile-img').click(function () {
         scrollTop: $(this).offset().top - 100
     }, 0, 'linear');
 });
+  
+// 함수 전체 묶어서 append 하면 호출
+function calllate() {
+    
+    //기본적으로 설명칸 닫기,흑백 만들기
+    for (let i = 1; i <= membernumbers; i++) {
+        $(`#Intro${i}`).hide();
+        $(`#profileImg${i}`).css('filter', 'grayscale(100%)');
+    }
+   
 
+    // 팀 설명 모두 숨긴후 ,선택한 팀원 설명만 토글하기
+    for (let i = 1; i <= membernumbers; i++) {
+       
+        $(`#profileImg${i}`).click(function () {
+            for (let j = 1; j <= membernumbers; j++) {
+                $(`#Intro${j}`).hide();
+                $(`#profileImg${j}`).css('filter', 'grayscale(100%)');
+            }
 
-  // 팀 설명 모두 숨긴후 ,선택한 팀원 설명만 토글하기
-  for (let i = 1; i <= 4; i++) {
-    $(`#profileImg${i}`).click(function () {
-      for (let j = 1; j <= 4; j++) {
-        $(`#Intro${j}`).hide();
-         $(`#Intro${i}`).toggle();
-      $('.profile-img').css('filter', 'grayscale(100%)');
-      $('#prfileImg${i}').css('filter', 'grayscale(0%)');
-      }
-    })
-  }
-
-
+            $(`#Intro${i}`).toggle();
+            $(`#profileImg${i}`).css('filter', 'grayscale(0%)');
+        })
+    }
+}
