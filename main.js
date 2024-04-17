8ec82f218eaa82675039be5d4923e5beb80b1236
yoon <pyh051920@gmail.com>
2024-04-17 21:08:55 +0900

    hovering+onlclick edit3

--- main.js
+++ main.js
@@ -13,19 +13,44 @@ $('.profile-img').click(function () {
         scrollTop: $(this).offset().top - 100
     }, 0, 'linear');
 });
+
+/********** 처음 시작할때 빈화면 시작하고 이미 열려있는 사람 누르면 내용 숨기는 코드 ***************************************************/
+// $('#johnProfileImg').click(() => {
+//     if($('#johnProfileName').css('text-decoration-line') == 'none') {
+//         $('.intro').hide();
+//         $('.profile-img').css('filter', 'grayscale(100%)');
+//         //클릭시 색상만듬
+//         $('#johnProfileImg').css('filter', 'grayscale(0%)');
+//         $('#johnIntro').toggle();
+//         //나머지 밑줄 해제
+//         $('#davidProfileName').css('text-decoration-line', 'none');
+//         $('#arialProfileName').css('text-decoration-line', 'none');
+//         $('#kevinProfileName').css('text-decoration-line', 'none');
+//         //밑줄 추가
+//         $('#johnProfileName').css('text-decoration', 'underline');
+//     //이미 보여진 사람 재 클릭시 사라지게
+//     } else{
+//         $('#johnIntro').hide();
+//         $('#johnProfileName').css('text-decoration', 'none');
+//     }
+// });
+
 /********** 클릭시 색상추가 및 밑줄 ***************************************************/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////// 밑줄 보이고 없애는 코드 수정이 필요. 너무 어거지로 해놔서 클래스던 아이디던 만들고 적용해야할듯 ///////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $('#johnProfileImg').click(() => {
     $('.intro').hide();
-    $('.profile-img').css('filter', 'grayscale(100%)');
-    //클릭시 색상만듬
-    $('#johnProfileImg').css('filter', 'grayscale(0%)');
-    $('#johnIntro').toggle();
-    //나머지 밑줄 해제
-    $('#davidProfileName').css('text-decoration-line', 'none');
-    $('#arialProfileName').css('text-decoration-line', 'none');
-    $('#kevinProfileName').css('text-decoration-line', 'none');
-    //밑줄 추가
-    $('#johnProfileName').css('text-decoration', 'underline');
+        $('.profile-img').css('filter', 'grayscale(100%)');
+        //클릭시 색상만듬
+        $('#johnProfileImg').css('filter', 'grayscale(0%)');
+        $('#johnIntro').toggle();
+        //나머지 밑줄 해제
+        $('#davidProfileName').css('text-decoration-line', 'none');
+        $('#arialProfileName').css('text-decoration-line', 'none');
+        $('#kevinProfileName').css('text-decoration-line', 'none');
+        //밑줄 추가
+        $('#johnProfileName').css('text-decoration', 'underline');
 });
 $('#davidProfileImg').click(() => {
     $('.intro').hide();
