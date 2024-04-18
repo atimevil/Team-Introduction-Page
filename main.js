229d8aba6bd1905ea153581d50d2c144cd97f721
yoon <pyh051920@gmail.com>
2024-04-18 15:12:39 +0900

    jquery클릭호버링기능수정

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 /*스크롤상태바*/
-$(window).scroll(function() {
+$(window).scroll(function () {
 
     var wins = $(this).scrollTop();
     var hei = $('.all-content').outerHeight();
     var hei2 = $(window).outerHeight();
     var height = hei - hei2;
     var bar = (wins / height) * 100;
-  
+
     $('#myBar').css('width', bar + '%');
-  
-  });
 
-  //몰라서 손 안댐
+});
 
-    $('#carouselExampleIndicators').carousel({
-        interval: 5000,
-        ride: "carousel"
-    });
+//몰라서 손 안댐
+
+$('#carouselExampleIndicators').carousel({
+    interval: 5000,
+    ride: "carousel"
+});
 
 $('.profile-img').click(function () {
     $('html, body').animate({
@@ -24,71 +24,54 @@ $('.profile-img').click(function () {
     }, 0, 'linear');
 });
 
-  
+
 // 함수 전체 묶어서 append 하면 호출
 function calllate() {
-    
+
     //기본적으로 설명칸 닫기,흑백 만들기
     for (let i = 1; i <= membernumbers; i++) {
         $(`#Intro${i}`).hide();
         $(`#profileImg${i}`).css('filter', 'grayscale(100%)');
     }
-   
+
 
     // 팀 설명 모두 숨긴후 ,선택한 팀원 설명만 토글하기
     for (let i = 1; i <= membernumbers; i++) {
-       
+
         $(`#profileImg${i}`).click(function () {
             for (let j = 1; j <= membernumbers; j++) {
                 $(`#Intro${j}`).hide();
                 $(`#profileImg${j}`).css('filter', 'grayscale(100%)');
-                
-                //index.html에서 클래스명 수정하고 쓰면 됩니다
-                //$('.profile-name').css('text-decoration', 'none');
+                /********추가함***** */
+                $(`#profileName${j}`).css('text-decoration', 'none');
             }
 
             $(`#Intro${i}`).toggle();
+            console.log("toggled" + i);
             $(`#profileImg${i}`).css('filter', 'grayscale(0%)');
-
-            // 동적 변수가 생겨서  for 문으로 반복문 만듦
-            // $('#kevinProfileImg').click(() => {
-            //     $('.intro').hide();
-            //     $('.profile-img').css('filter', 'grayscale(100%)');
-            //     $('#kevinProfileImg').css('filter', 'grayscale(0%)');
-            //     $('.profile-name').css('text-decoration', 'none');
-            //     $('#kevin').css('text-decoration', 'underline');
-            //     $('#kevinIntro').toggle();
-            // });
-            
+            console.log(`#profileName${i}`);
+            $(`#profileName${i}`).css('text-decoration', 'underline');
         })
+        /********추가함***** *///마우스 호버링 기능
+        $(`#profileBox${i}`).mouseover(function () {
+            $(this).css('transform', 'scale(1.2)');
+            if ($(`#profileName${i}`).css('text-decoration-line') == 'none') {
+                $(`#profileImg${i}`).css('filter', 'grayscale(70%)');
+            } else {
+                $(`#profileImg${i}`).css('filter', 'grayscale(0%)');
+            }
+        });
+        $(`#profileBox${i}`).mouseout(function () {
+            $(this).css('transform', 'scale(1.0)');
+            if ($(`#profileName${i}`).css('text-decoration-line') == 'none') {
+                $(`#profileImg${i}`).css('filter', 'grayscale(100%)');
+            } else {
+                $(`#profileImg${i}`).css('filter', 'grayscale(0%)');
+            }
+        });
     }
-    
+
 }
-
-/********** 클릭시 색상추가 및 밑줄 **************************************************/
-// $('#johnProfileImg').click(() => {
-//     $('#johnProfileImg').css('filter', 'grayscale(0%)');
-//     $('#johnIntro').toggle();
-//     $('#johnProfileName').css('text-decoration', 'underline');
-// });
-
-/********** 마우스 호버링 기능 ***************************************************/
-// $('#johnProfileBox').mouseover(function() {
-//     $(this).css('transform', 'scale(1.2)');
-//     if($('#johnProfileName').css('text-decoration-line') == 'none') {
-//         $('#johnProfileImg').css('filter', 'grayscale(70%)');
-//     } else{
-//         $('#johnProfileImg').css('filter', 'grayscale(0%)');
-//     }
-// });
-// $('#johnProfileBox').mouseout(function() {
-//     $(this).css('transform', 'scale(1.0)');
-//     if ($('#johnProfileName').css('text-decoration-line') == 'none') {
-//         $('#johnProfileImg').css('filter', 'grayscale(100%)');
-//     } else{
-//         $('#johnProfileImg').css('filter', 'grayscale(0%)');
-//     }
-// });
 
 $('.wip').click(() => {
     alert('죄송합니다. 아직 준비 중인 페이지입니다.');
